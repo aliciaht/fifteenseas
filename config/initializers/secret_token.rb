e779a16141d499ab0b024ae95b5e2a112ea2f0a2
Alicia Tan <aliciaht@gmail.com>
2013-09-03 18:18:59 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fifteenseas::Application.config.secret_token = '12f0e12a675773c60fb55ecd469c7b00d39b4882eeab10bfbd6205791531544fab960d520d9a84ba6c03b7c1019f51551b6b445aba9a1b46a5097a763e3eb2f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fifteenseas::Application.config.session_store :cookie_store, key: '_fifteenseas_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fifteenseas::Application.config.session_store :active_record_store
